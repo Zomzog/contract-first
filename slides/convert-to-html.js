b66ce41b089d096633fde13805852eba8aa1b165
Thibault Duperron <zomzog@gmail.com>
2019-12-01 22:57:01 +0100

    Draft

---
+++ slides/convert-to-html.js
@@ -0,0 +1,26 @@
+'use strict';
+const map = require('map-stream');
+
+// Load asciidoctor.js and asciidoctor-reveal.js
+const  asciidoctor = require('asciidoctor.js')();
+var asciidoctorRevealjs = require('asciidoctor-reveal.js');
+asciidoctorRevealjs.register()
+
+/**
+ * This function receives a stream with different metadata on asciidoc files. For example
+ *
+ * The aim is to replace adoc extension by html extensions
+ *
+ * @returns {stream}
+ */
+module.exports = function () {
+
+  return map((file, next) => {
+    // Convert the document 'presentation.adoc' using the reveal.js converter
+    const attributes = {'revealjsdir': 'node_modules/reveal.js@'};
+    const options = {safe: 'safe', backend: 'revealjs', attributes: attributes};
+    asciidoctor.convertFile(file.path, options);
+
+    next(null, file);
+  });
+};
